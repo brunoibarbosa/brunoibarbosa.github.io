3e76b8f734ad4e4b4970375c710e9f27466ff03b
Bruno Inacio <brunoinacio1590@gmail.com>
2020-11-04 15:57:43 -0300

    Remake site completo

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Site Pessoal
 
-Aqui se encontra mais informações sobre mim e os meus projetos.
+Foi proposto durante o 1° semestre do curso Sistemas para Internet, da Fatec Jahu, desenvolver um site pessoal onde será possível encontrar mais informações sobre mim e os meus projetos.
+
+🔗 É possível acessar o site [por aqui](http://201.55.33.89/si/2019/0200831911004/) ou no próprio GitHub Pages clicando [aqui](https://brunoibarbosa.github.io/).
--- assets/js/main.js
+++ assets/js/main.js
@@ -10,9 +10,12 @@ document.addEventListener('DOMContentLoaded', () => {
 function headerSticky() {
     const navbarSticky = document.querySelector('#main-header')
 
-    window.addEventListener('scroll', () => {
+    window.addEventListener('DOMContentLoaded', addSticky)
+    window.addEventListener('scroll', addSticky)
+
+    function addSticky() {
         pageYOffset > 0 ? navbarSticky.classList.add('sticky') : navbarSticky.classList.remove('sticky')
-    })
+    }
 }
 headerSticky()
 
@@ -22,31 +25,99 @@ function showMenu() {
     menuToggle.addEventListener('click', (e) => {
         e.target.classList.toggle('active')
     })
+
+    window.addEventListener('resize', () => menuToggle.classList.remove('active'))
 }
 showMenu()
 
-// Recupera repositórios do Github
-async function getRepoGithub() {
-    const repositorios = await axios('https://api.github.com/users/brunoibarbosa/repos')
-    return repositorios.data
+// Identifica seção ativa e marca no menu
+function activeSection() {
+    const sections = [...document.querySelectorAll('main > section')]
+    const sectionDistance = sections.map(sec => {
+        const itemMenu = document.querySelector(`.item-menu.${sec.id} > a`)
+        const distanceActive = sec.offsetTop + (sec.offsetHeight / 2)
+        return { itemMenu, distanceActive }
+    })
+
+    document.addEventListener('scroll', checkDistance)
+
+    function checkDistance() {
+        for (const i in sectionDistance) {
+            const { distanceActive, itemMenu } = sectionDistance[i]
+
+            if (distanceActive >= scrollY) {
+                try {
+                    document.querySelector('.item-menu > a.active').classList.remove('active')
+                } catch (e) { }
+
+                itemMenu.classList.add('active')
+                break
+            }
+        }
+    }
+    checkDistance()
 }
 
-// Cria card e coloca dentro do container passado como parâmetro
-async function createCard(classContainer) {
-    const container = document.querySelector(classContainer)
-    const repos = await getRepoGithub()
+// Recupera repositórios do Github
+async function getRepoGithub() {
+    try {
+        const { data } = await axios('https://api.github.com/users/brunoibarbosa/repos')
+        return data
+    } catch (e) {
+        throw new Error(e)
+    }
+}
 
+// Cria os cards que vão dentro do carousel
+async function createCards() {
+    try {
+        const repos = await getRepoGithub()
+        const cards = []
 
-    for (const key in repos) {
-        const section = document.createElement('section')
-        section.classList.add('card')
+        for (const key in repos) {
+            const { login } = repos[key].owner
+            const { name, description, default_branch, html_url } = repos[key]
 
-        const img = document.createElement('img')
+            const section = document.createElement('section')
+            section.classList.add('card')
 
-        img.setAttribute('src', `https://raw.githubusercontent.com/brunoibarbosa/${repos[key].name}/main/.github/logo.svg`)
+            const img = document.createElement('img')
+            img.setAttribute('src', `https://raw.githubusercontent.com/${login}/${name}/${default_branch}/.github/logo.svg`)
 
-        section.appendChild(img)
-        container.appendChild(section)
+            const p = document.createElement('p')
+            p.textContent = description
+
+            const a = document.createElement('a')
+            a.setAttribute('href', html_url)
+            a.setAttribute('target', '_blank')
+            a.textContent = 'Ver mais no Github'
+
+            section.appendChild(img)
+            section.appendChild(p)
+            section.appendChild(a)
+            cards.push(section)
+        }
+        return cards
+
+    } catch (e) {
+        throw new Error(e)
+    }
+}
+
+// Cria o carousel e coloca dentro do container passado como parâmetro
+async function createCarousel(classContainer) {
+    try {
+        const container = document.querySelector(classContainer)
+        const cards = await createCards()
+        cards.forEach(card => {
+            container.appendChild(card)
+        })
+
+        // Quando a primeira imagem carregar, é calculado o tamanho das seções
+        container.querySelector('.card > img').addEventListener('load', activeSection)
+
+    } catch (e) {
+        throw new Error(e)
     }
 
     new Glider(document.querySelector('.js-carousel'), {
@@ -60,7 +131,23 @@ async function createCard(classContainer) {
             prev: ".js-carousel--prev",
             next: ".js-carousel--next",
         },
+        scrollLockDelay: 50,
+        responsive: [
+            {
+                breakpoint: 900,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3
+                }
+            }
+        ]
     });
-
 }
-createCard('.repo-github .js-carousel')
+createCarousel('.repo-github .js-carousel')
